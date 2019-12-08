0f4ed78d33d6b5c798b5762b5e535b4274818b9b
btanguturu <46464320+btanguturu@users.noreply.github.com>
2019-12-07 19:22:30 -0800

    added in new buttons

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const data = [
 
   { id: '', letter: 'Oh ow!', src: 'https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/Oh%20Ow.mp3' },
 
-  { id: '', letter: "That's all Folks", src: "https://github.com/btanguturu/drumkit/blob/master/assets/audio/That's%20All%20Folks.mp3" },
+  { id: '', letter: "That's all Folks", src: "https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/That's%20All%20Folks.mp3" },
 
   { id: '', letter: "What's up doc", src: "https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/What's%20up%20Doc.mp3" },
 
@@ -23,11 +23,11 @@ const data = [
 
   { id: '', letter: 'You are right', src: 'https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/You%20are%20right.mp3' },
 
-  { id: '', letter: 'You are right', src: 'https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/You%20are%20right.mp3' },
+  { id: '', letter: 'Hello', src: 'https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/hello.mp3' },
 
-  { id: '', letter: 'You are right', src: 'https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/You%20are%20right.mp3' },
-  
-  { id: '', letter: 'You are right', src: 'https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/You%20are%20right.mp3' }
+  { id: '', letter: 'Sorry',  src: 'https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/im%20sorry.mp3' },
+
+  { id: '', letter: 'le meow', src: 'https://raw.githubusercontent.com/btanguturu/drumkit/master/assets/audio/meow.mp3' }
 ]
 
 class DrumPad extends React.Component {
